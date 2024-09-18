523a453403d2247bbf3cf4795754196c830336e5
sumitrapermal <sumitrapermal@gmail.com>
2024-09-18 17:41:39 +0530

    Add files via upload

---
+++ Alpha.py
@@ -0,0 +1,12 @@
+#create a Alphabet (A-Z) password
+#Python has a built-in module that you can use to make random numbers.
+
+import random
+password="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+length=int(input("Enter the length of the password : "))
+
+#join() method takes all items in an iterable and joins them into one string.
+#The sample() method returns a value with a specified number of randomly selected items from a sequence
+
+a="".join(random.sample(password,length))
+print(f"the password is : {a}")
